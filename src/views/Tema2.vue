28dbf7a1326e07bfa54de7e2f04930783be8a63e
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-07-23 13:24:55 -0500

    Ajustes

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -53,24 +53,24 @@
             //figcaption Leyenda de la imagen
         .tarjeta-avatar.infografica3.mb-5(x="17%" y="67%")          
           .tarjeta.color-acento-botones.p-4
-            h4.text-center Fase 1:
-            p.text-center Empatía.<br><br>Investigar las necesidades de tus usuarios. Se trata de entender empáticamente el problema que está tratando de resolver. Por lo tanto, esta fase suele comenzar por la investigación del usuario o consumidor.
+            h4.text-center Fase 1
+            p.text-center Empatía<br><br>Investigar las necesidades de tus usuarios. Se trata de entender empáticamente el problema que está tratando de resolver. Por lo tanto, esta fase suele comenzar por la investigación del usuario o consumidor.
         .tarjeta-avatar.infografica3.mb-5(x="17%" y="33%")          
           .tarjeta.color-acento-botones.p-4            
-            h4.text-center Fase 2:
-            p.text-center Definir.<br><br>Identificar las necesidades y los problemas de sus usuarios. Es hora de poner sobre la mesa la información recopilada durante la primera etapa. Se analizan todas las observaciones y se sintetizan para definir los problemas centrales que el equipo ha identificado.
+            h4.text-center Fase 2
+            p.text-center Definir<br><br>Identificar las necesidades y los problemas de sus usuarios. Es hora de poner sobre la mesa la información recopilada durante la primera etapa. Se analizan todas las observaciones y se sintetizan para definir los problemas centrales que el equipo ha identificado.
         .tarjeta-avatar.infografica3.mb-5(x="47%" y="15%")         
           .tarjeta.color-acento-botones.p-4            
-            h4.text-center Fase 3:
-            p.text-center Idear.<br><br>En esta fase, se trata de generar cuantas ideas sea posible. Se trata de “pensar fuera de la caja”, buscar formas alternativas de ver el problema e identificar soluciones innovadoras para el planteamiento del problema.
+            h4.text-center Fase 3
+            p.text-center Idear<br><br>En esta fase, se trata de generar cuantas ideas sea posible. Se trata de “pensar fuera de la caja”, buscar formas alternativas de ver el problema e identificar soluciones innovadoras para el planteamiento del problema.
         .tarjeta-avatar.infografica3.mb-5(x="78%" y="32%")          
           .tarjeta.color-acento-botones.p-4            
-            h4.text-center Fase 4:
-            p.text-center Prototipar.<br><br>Construir soluciones. Inicialmente se identifica la mejor solución para cada problema encontrado. A continuación, el equipo tiene que construir versiones económicas y muy primitivas de lo que sería el producto final, aunque sea en papel.
+            h4.text-center Fase 4
+            p.text-center Prototipar<br><br>Construir soluciones. Inicialmente se identifica la mejor solución para cada problema encontrado. A continuación, el equipo tiene que construir versiones económicas y muy primitivas de lo que sería el producto final, aunque sea en papel.
         .tarjeta-avatar.infografica3.mb-5(x="77%" y="67%")          
           .tarjeta.color-acento-botones.p-4
-            h4.text-center Fase 5:
-            p.text-center Testear o probar.<br><br>Probar los prototipos es la última de las fases del proceso de Design Thinking. Existe una figura –la de los evaluadores–, que se encarga de probar rigurosamente los prototipos. Por lo tanto, es posibles volver a las etapas anteriores para realizar más iteraciones, alteraciones y refinamientos.
+            h4.text-center Fase 5
+            p.text-center Testear o probar<br><br>Probar los prototipos es la última de las fases del proceso de Design Thinking. Existe una figura –la de los evaluadores–, que se encarga de probar rigurosamente los prototipos. Por lo tanto, es posibles volver a las etapas anteriores para realizar más iteraciones, alteraciones y refinamientos.
 </template>
 
 <script>
